3185ea6a228854bbabc0bc440f3ba5094f583df5
tastyghost <tastyghost@gmail.com>
2020-10-03 17:01:00 +0200

    Added ProjectileEngineManager to the scene context

--- Assets/Scripts/Installers/PlayerInstaller.cs
+++ Assets/Scripts/Installers/PlayerInstaller.cs
@@ -9,13 +9,13 @@ public class PlayerInstaller : MonoInstaller
      [SerializeField] private GameObject _projectileEnginePrefab = default;
 
      [SerializeField] private GameObject _pickableSeedPrefab = default;
-     
+     public GameObject projectileEngineManager=default;
      public override void InstallBindings()
      {
           Container.Bind<Player>().FromComponentInHierarchy().AsSingle();
           Container.Bind<AmmunitionStorage>().FromComponentInNewPrefab(_ammunitionStoragePrefab).AsSingle().NonLazy();
 
-          Container.Bind<ProjectileEngineManager>().AsSingle();
+          Container.Bind<ProjectileEngineManager>().FromComponentInNewPrefab(projectileEngineManager).AsSingle();
           Container.BindMemoryPool<ProjectileEngine, ProjectileEngine.Pool>().FromComponentInNewPrefab(_projectileEnginePrefab).AsCached();
 
           Container.Bind<PickableSeedManager>().AsSingle();
--- Assets/Scripts/ProjectileEngineManager.cs
+++ Assets/Scripts/ProjectileEngineManager.cs
@@ -2,11 +2,14 @@
 using UnityEngine;
 using Zenject;
 
-public class ProjectileEngineManager
+public class ProjectileEngineManager : MonoBehaviour
 {
+    public float cislo;
+    public GameObject fireExplosion;
     [Inject] private readonly ProjectileEngine.Pool _projectileEnginePool = default;
     [Inject] private readonly PickableSeedManager _pickableSeedManager = default;
     
+ 
     public void SpawnSeed(Vector3 position, Quaternion rotation, ProjectileType projectileType)
     {
         var seed = _projectileEnginePool.Spawn(position, rotation, projectileType);
@@ -22,6 +25,7 @@ public class ProjectileEngineManager
         {
             case ProjectileType.FireSeed:
                 Debug.Log("Fire");
+                //GameObject.Instantiate(fireExplosion,collisionHit,rotation);
                 break;
             case ProjectileType.PoisonSeed:
                 Debug.Log("Poison");
