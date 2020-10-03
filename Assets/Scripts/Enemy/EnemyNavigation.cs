8b977a63636cc84b0e63fd08b0e59d6b5f166ef3
Jan Slifka <jan.slifka@gmail.com>
2020-10-03 09:37:00 +0200

    Add enemy navigation

---
+++ Assets/Scripts/Enemy/EnemyNavigation.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyNavigation : MonoBehaviour
+{
+    public Transform target;
+
+    private NavMeshAgent _navMeshAgent;
+
+    private void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+
+        _navMeshAgent.updateRotation = false;
+        _navMeshAgent.updateUpAxis = false;
+    }
+
+    private void Update()
+    {
+        if (target != null)
+        {
+            _navMeshAgent.SetDestination(target.position);
+        }
+        
+        RotateTowardsDestination();
+    }
+
+    private void RotateTowardsDestination()
+    {
+        var vectorToTarget = _navMeshAgent.destination - transform.position;
+        var angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
