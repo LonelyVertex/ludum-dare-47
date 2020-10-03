b570ece5f9961824eaefa32d24ae74d443a485a0
Jakub Mirovský <mirovsky.jakub@gmail.com>
2020-10-03 12:14:42 +0200

    add ammunition storage

---
+++ Assets/Scripts/Ammo/AmmunitionStorage.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class AmmunitionStorage : MonoBehaviour
+{
+    [SerializeField] private int _maxAmmunition = default;
+    [SerializeField] private int _currentAmmoCount = default;
+
+    public int maxAmmunitionCount => _maxAmmunition;
+    public int currentAmmunitionCount => _currentAmmoCount;
+    
+    public bool HasAmmo()
+    {
+        return _currentAmmoCount > 0;
+    }
+
+    public bool IsFull()
+    {
+        return _currentAmmoCount == _maxAmmunition;
+    }
+    
+    public bool GetAmmo()
+    {
+        if (!HasAmmo())
+        {
+            return false;
+        }
+        
+        _currentAmmoCount--;
+        return true;
+    }
+
+    public bool PutAmmo()
+    {
+        if (IsFull())
+        {
+            return false;
+        }
+
+        _currentAmmoCount++;
+        return true;
+    }
+}
--- Assets/Scripts/Installers/PlayerInstaller.cs
+++ Assets/Scripts/Installers/PlayerInstaller.cs
@@ -1,9 +1,13 @@
+using UnityEngine;
 using Zenject;
 
 public class PlayerInstaller : MonoInstaller
 {
+     [SerializeField] private AmmunitionStorage _ammunitionStoragePrefab = default;
+     
      public override void InstallBindings()
      {
           Container.Bind<Player>().FromComponentInHierarchy().AsSingle();
+          Container.Bind<AmmunitionStorage>().FromComponentInNewPrefab(_ammunitionStoragePrefab).AsSingle().NonLazy();
      }
 }
