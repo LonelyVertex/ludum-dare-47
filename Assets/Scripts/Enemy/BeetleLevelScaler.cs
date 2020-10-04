87b132bf3101442a0b07e3399e1096648da5a3ac
Jan Slifka <jan.slifka@gmail.com>
2020-10-04 20:33:41 +0200

    Add beetle

---
+++ Assets/Scripts/Enemy/BeetleLevelScaler.cs
@@ -0,0 +1,14 @@
+﻿public class BeetleLevelScaler : EnemyLevelScaler
+{
+    public Health health;
+    public EnemyMeleeAttack attack;
+    
+    private const int BaseHealth = 150;
+    private const int BaseAttackDamage = 40;
+    
+    public override void SetLevel(int level)
+    {
+        health.SetMaxHealth(Scaler.ScaleHealth(BaseHealth, level));
+        attack.damage = Scaler.ScaleDamage(BaseAttackDamage, level);
+    }
+}
