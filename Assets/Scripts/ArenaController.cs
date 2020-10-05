b71683f9a5ff90617d6d02a69b331ec7ee6bcf9c
tastyghost <tastyghost@gmail.com>
2020-10-05 12:20:38 +0200

    Revert "Fix boss wave game end"
    This reverts commit 6941b43286ec2dddb59c1bdbce4d6249e54a5457.

--- Assets/Scripts/ArenaController.cs
+++ Assets/Scripts/ArenaController.cs
@@ -44,7 +44,6 @@ public class ArenaController : MonoBehaviour
 
     [Inject] DiContainer _container;
     [Inject] private GameState _gameState;
-    private int _bossSpawnPoints;
 
     #region Gizmos
 
@@ -103,16 +102,19 @@ public class ArenaController : MonoBehaviour
 
         waveSpawnedEvent?.Invoke();
 
+        if (wave.bossWave)
+        {
+            bossWaveSpawned?.Invoke();
+        }
+
         foreach (var spawnPoint in wave.spawnPoints)
         {
-            StartCoroutine(SpawnSpawnPoint(spawnPoint, wave.bossWave));
+            StartCoroutine(SpawnSpawnPoint(spawnPoint));
         }
     }
 
-    IEnumerator SpawnSpawnPoint(SpawnPoint spawnPoint, bool bossWave)
+    IEnumerator SpawnSpawnPoint(SpawnPoint spawnPoint)
     {
-        if (bossWave) AddBossSpawnPoint();
-        
         yield return new WaitForSeconds(spawnPoint.spawnPointDelay);
 
         var spawnDelay = new WaitForSeconds(spawnPoint.spawnDelay);
@@ -125,8 +127,6 @@ public class ArenaController : MonoBehaviour
             SpawnEnemy(spawnPoint);
             yield return spawnDelay;
         }
-        
-        if (bossWave) RemoveBossSpawnPoint();
     }
 
     void SpawnEnemy(SpawnPoint spawnPoint)
@@ -137,19 +137,5 @@ public class ArenaController : MonoBehaviour
         enemy.GetComponent<EnemyLevelScaler>().SetLevel(_gameState.level);
 
         enemySpawnedEvent?.Invoke();
-    }
-
-    void AddBossSpawnPoint()
-    {
-        _bossSpawnPoints++;
-    }
-
-    void RemoveBossSpawnPoint()
-    {
-        _bossSpawnPoints--;
-        if (_bossSpawnPoints <= 0)
-        {
-            bossWaveSpawned?.Invoke();
-        }
     }
 }
